d3b6b7f18c71e1fa98e2b78b29bdbe585c2f9efb
Keijiro Takahashi <keijiro@gmail.com>
2021-08-18 00:14:58 +0900

    ReadOnlySpan workaround on Unity 2021.2

--- jp.keijiro.unityspantest/Editor/SpanTestEditor.cs
+++ jp.keijiro.unityspantest/Editor/SpanTestEditor.cs
@@ -5,8 +5,21 @@ using UnityEditor;
 [CustomEditor(typeof(SpanTest))]
 sealed class SpanTestEditor : Editor
 {
+    #if UNITY_2021_2_OR_NEWER
+
+    public static int ReadSpan(ReadOnlySpan<int> span)
+    {
+        var temp = (Span<int>)(stackalloc int[span.Length]);
+        span.CopyTo(temp);
+        return temp[0];
+    }
+
+    #else
+
     public static int ReadSpan(ReadOnlySpan<int> span)
       => span[0];
+
+    #endif
 
     public override void OnInspectorGUI()
     {
