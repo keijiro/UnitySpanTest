18c92f057de46e8d19b1c79df2f77b00b57aa74e
Keijiro Takahashi <keijiro@gmail.com>
2021-10-02 22:41:08 +0900

    Now we can use ReadOnlySpan with .NET Framework

--- jp.keijiro.unityspantest/Editor/SpanTestEditor.cs
+++ jp.keijiro.unityspantest/Editor/SpanTestEditor.cs
@@ -5,21 +5,8 @@ using UnityEditor;
 [CustomEditor(typeof(SpanTest))]
 sealed class SpanTestEditor : Editor
 {
-#if NET_STANDARD_2_0 || NET_STANDARD_2_1
-
     public static int ReadSpan(ReadOnlySpan<int> span)
       => span[0];
-
-#else
-
-    public static int ReadSpan(ReadOnlySpan<int> span)
-    {
-        var temp = (Span<int>)(stackalloc int[span.Length]);
-        span.CopyTo(temp);
-        return temp[0];
-    }
-
-#endif
 
     public override void OnInspectorGUI()
     {
